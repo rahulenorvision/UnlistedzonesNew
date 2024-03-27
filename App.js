f465d1b50686816b5698224bf9efd673e3d20220
rahul9396 <145899704+rahul9396@users.noreply.github.com>
2024-03-27 13:27:29 +0530

    Changes made

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './src/Home.js';
+
+function App() {
+  return (
+    <div className='page'>
+     <Home/>
+</div>
+  );
+}
+export default App;
+
+
+
+
---
+++ Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Unlisted.css';
+import Nav from './src/Component/Nav.js';
+import Content from './src/Component/Content.js';
+
+const Home = () => {
+    return (
+        <div>
+            <p>Working</p>
+            <Nav/>
+            <Content/>
+        </div>
+    );
+}
+
+export default Home;
